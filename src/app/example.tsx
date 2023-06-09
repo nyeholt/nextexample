64ffdbbd755c3b41d10330d54613b202b0f2e79b
Marcus Nyeholt <marcus@symbiote.com.au>
2023-06-09 11:03:57 +1000

    added analytics to test

---
+++ src/app/example.tsx
@@ -0,0 +1,17 @@
+
+import './base.css';
+
+import './mytheme.css';
+
+export default function Example() {
+  return (
+    <main className="">
+      <div className="">
+        <div>
+        <button className="button">Button</button>
+        </div>
+      </div>
+
+    </main>
+  )
+}
---
+++ src/app/example/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+import '../base.css';
+
+
+export default function Example() {
+  return (
+    <main >
+      <div >
+        <div>
+        <button className="button">Example page button</button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+
